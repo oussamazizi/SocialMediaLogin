c03d846118bbe4715e729f335a4630739ebdbb1d
azizi <louayerrahem@gmail.com>
2020-07-23 20:59:42 +0100

    menu

--- app/src/main/java/com/example/azizi/splash/MainActivity.kt
+++ app/src/main/java/com/example/azizi/splash/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.azizi.splash
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,20 +26,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        log_out_button.setOnClickListener {
-            AuthUI.getInstance().signOut(this)
-                    .addOnCompleteListener(OnCompleteListener {
-                        log_out_button.isEnabled = false
-                        showSignInOptions()
-                    }).addOnFailureListener(OnFailureListener {
-                        Toast.makeText(this,"${it.message}", Toast.LENGTH_SHORT).show()
-                    })
-        }
 
         providers = Arrays.asList(
                 AuthUI.IdpConfig.EmailBuilder().build(), // Email builder
                 AuthUI.IdpConfig.PhoneBuilder().build(), // Phone builder
-                AuthUI.IdpConfig.FacebookBuilder().build(), // Facebook builder
+                //AuthUI.IdpConfig.FacebookBuilder().build(), // Facebook builder
                 AuthUI.IdpConfig.GoogleBuilder().build() // Google builder
         )
 
@@ -62,9 +55,10 @@ class MainActivity : AppCompatActivity() {
                 // Get user
                 var user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
                 // show email on toast
-                Toast.makeText(this, "${user!!.email}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${user!!.photoUrl}", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
                 // Get button signout
-                log_out_button.isEnabled = true
             }
             else {
                 Toast.makeText(this, "${response.error!!.message}", Toast.LENGTH_SHORT).show()
